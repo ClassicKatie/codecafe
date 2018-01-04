06ccf86e331b2298b8722702edf5d88774701c36
Katie Ford <katherine.ford@dreamhost.com>
2018-01-04 11:54:25 -0800

    changes to authorization

---
+++ basic.py
@@ -0,0 +1,33 @@
+#!/user/bin/env python
+import get_auth # import our helper file for accessing twitter
+
+"""
+What this file provides:
+
+"""
+
+def write(message):
+    """
+
+    :param message: string message to write
+    :return:
+    """
+    api = get_auth.connect()
+    api.update_status(message)
+    return
+
+
+def main():
+    """
+    This function runs our twitter bot script.
+    This is what our cron job will call on the interval
+
+    :return: None
+    """
+    print("Debug!")
+    write("debug")
+    return
+
+
+if __name__ == '__main__': # boilerplate for running the script
+    exit(main())
